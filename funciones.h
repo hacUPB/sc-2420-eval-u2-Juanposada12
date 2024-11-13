0be9c50b55d7f96b190466685414121b74fa7277
Juanposada12 <juan.posadar@upb.edu.co>
2024-11-13 07:13:01 -0500

    Add files via upload

---
+++ funciones.h
@@ -0,0 +1,8 @@
+// funciones.h
+#ifndef FUNCIONES_H
+#define FUNCIONES_H
+
+void saludar();  // Función para saludar
+int suma(int a, int b);  // Función para sumar dos números
+
+#endif // FUNCIONES_H
